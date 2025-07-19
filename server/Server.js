8586bc273d2d3983678bc9774d89cb5f74e8bed0
shubham.sc <shubham@skills.cafe>
2025-07-19 16:51:00 +0530

    cors

--- server/Server.js
+++ server/Server.js
@@ -20,15 +20,24 @@ dbConnection();
 // setup middlewares
 app.use(express.json());
 app.use(cookieParser());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://grocee-frontend.onrender.com",
+  "https://grocee-repo-k82k.vercel.app",
+];
 
 app.use(
   cors({
-    origin:
-      /^(http:\/\/localhost:5173|https:\/\/grocee-frontend\.onrender\.com)$/,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
-
 // mouting routes
 app.use("/api/user", userRouter);
 app.use("/api/seller", sellerRouter);
